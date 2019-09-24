c7ea7695df90e8df79f1a1880f216fdb5d456278
Tony D'Addeo <tony@askhr.com>
2019-09-24 12:33:03 -0500

    Improve typing in useQuill

--- src/hooks/use-quill.ts
+++ src/hooks/use-quill.ts
@@ -1,9 +1,8 @@
 import { useCallback, useRef } from 'react'
 import Quill, { DeltaStatic, QuillOptionsStatic } from 'quill'
 
-
 export interface Options {
-  configurationOptions: QuillOptionsStatic 
+  configurationOptions: QuillOptionsStatic
   textChange: (delta: DeltaStatic) => void
   initialValue: DeltaStatic
 }
@@ -12,10 +11,10 @@ export default ({
   configurationOptions,
   initialValue,
   textChange
-}: Options) => {
+}: Options): [(node: Element | null) => void, Quill | null]=> {
   const instance = useRef<Quill | null>(null)
 
-  const container = useCallback(node => {
+  const container = useCallback((node: Element | null) => {
     if (node !== null && instance.current === null) {
       instance.current = new Quill(node, configurationOptions)
       instance.current.on('text-change', (delta, oldDelta, source) => source === "user" && textChange(oldDelta.compose(delta)))
@@ -24,5 +23,5 @@ export default ({
     }
   }, [configurationOptions, textChange, initialValue])
 
-  return container
+  return [container, instance.current]
 }
